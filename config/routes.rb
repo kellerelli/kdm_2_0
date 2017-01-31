ea681dd68df8187028e4cd1989501992fcac88e9
rugmissile <carson.chu@gmail.com>
2017-01-30 23:18:04 -0500

    committing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get 'show' => 'home#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :monsters, :only =>[:index]
 end
